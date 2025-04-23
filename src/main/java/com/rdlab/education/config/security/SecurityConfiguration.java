7daa8659be0b019672182f180e3cc15472a66d89
Bekzat Zharylkassyn <bekzat.jobs@gmail.com>
2025-04-23 16:40:05 +0500

    fix cors

--- src/main/java/com/rdlab/education/config/security/SecurityConfiguration.java
+++ src/main/java/com/rdlab/education/config/security/SecurityConfiguration.java
@@ -90,7 +90,6 @@ public class SecurityConfiguration {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("*"));
         configuration.setAllowedOriginPatterns(List.of("*"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
