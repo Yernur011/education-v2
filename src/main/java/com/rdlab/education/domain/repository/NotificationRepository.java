3f5e10901638438def0d0d0bc0978626fa39a67b
yernurskiy <tka99.ru@gmail.com>
2025-05-05 14:01:46 +0500

    Refactor `findByUsernameAndRead` with custom JPQL query.
    Replaced the method declaration with a custom JPQL query to improve flexibility and readability. Added the `@Query` annotation and parameter bindings for better control over query execution.

--- src/main/java/com/rdlab/education/domain/repository/NotificationRepository.java
+++ src/main/java/com/rdlab/education/domain/repository/NotificationRepository.java
@@ -4,10 +4,15 @@ import com.rdlab.education.domain.entity.edu.Notification;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
     Page<Notification> findByUsername(String username, Pageable pageable);
+    @Query(value = "SELECT n from Notification n " +
+            "where n.username = :username " +
+            "AND n.isRead = :read")
 
-    Page<Notification> findByUsernameAndRead(String username, boolean read, Pageable pageable);
+    Page<Notification> findByUsernameAndRead(@Param("username") String username, @Param("read") boolean read, Pageable pageable);
 }
